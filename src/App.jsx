b10f5087306d899cb1f758428122998d1b419140
maundu55 <marymaundu.mm@gmail.com>
2025-01-17 12:58:45 +0300

    A React JS ecommerce cart project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
