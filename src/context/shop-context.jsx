0ae958126881aa6c3f89f1776ae7a1785e9d8641
maundu55 <marymaundu.mm@gmail.com>
2025-01-18 17:51:06 +0300

    add to cart function

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,29 +1,53 @@
 import { useState, createContext } from "react";
-import { PRODUCTS } from '../products'
+import { PRODUCTS } from '../products';
 
 export const ShopContext = createContext(null);
 
-const getDefaultCart =()=>{
-    let cart ={};
-    for (let i =1; i<PRODUCTS.length; i++){
-        cart[i]=0;
+const getDefaultCart = () => {
+    let cart = {};
+    for (let i = 1; i <= PRODUCTS.length; i++) {
+        cart[i] = 0;
     }
     return cart;
-}
+};
 
-export const ShopContextProvider =(props)=>{
+export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    const addToCart = (itemId) =>{
-        setCartItems((prev)=>({...prev, [itemId]: prev[itemId] + 1}))
-    };
-    const removeFromCart = (itemId) =>{
-        setCartItems((prev)=>({...prev, [itemId]: prev[itemId] - 1}))
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     };
 
-    const contextValue ={ cartItems, addToCart, removeFromCart };
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: Math.max(prev[itemId] - 1, 0) }));
+    };
 
-    return <ShopContext.Provider value={contextValue} >
-        {props.children}
-    </ShopContext.Provider>
-}
+    const updateCartItemCount = (itemId, count) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: count }));
+    };
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
+                totalAmount += cartItems[item] * itemInfo.price;
+            }
+        }
+        return totalAmount;
+    };
+
+    const contextValue = {
+        cartItems,
+        addToCart,
+        removeFromCart,
+        updateCartItemCount,
+        getTotalCartAmount,
+    };
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    );
+};
--- src/pages/shop/Product.jsx
+++ src/pages/shop/Product.jsx
@@ -3,8 +3,9 @@ import { ShopContext } from '../../context/shop-context';
 
 const Product = (props) => {
     const { id, productName, price, productImage } = props.data;
-  const { addToCart } = useContext (ShopContext)
+  const { addToCart, cartItems } = useContext (ShopContext)
 
+  const cartItemAmount = cartItems[id];
   return (
     <div className='product' > 
         <img src={productImage} />
@@ -15,7 +16,7 @@ const Product = (props) => {
             <p>${price}</p>
         </div>
         <button className='addToCartBttn' onClick={()=> addToCart(id)}>
-          Add To Cart</button>
+          Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}</button>
     </div>
   )
 }
